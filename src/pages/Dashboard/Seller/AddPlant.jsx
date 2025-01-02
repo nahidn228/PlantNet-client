ac9e65836bc9d5e35d5900ad9d2ea3039475db60
Nahid Hasan <nahidn228@gmail.com>
2025-01-02 23:09:53 +0600

    get add plant data

--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -1,7 +1,43 @@
-import { Helmet } from 'react-helmet-async'
-import AddPlantForm from '../../../components/Form/AddPlantForm'
+import { Helmet } from "react-helmet-async";
+import { imageUpload } from "../../../api/utils";
+import AddPlantForm from "../../../components/Form/AddPlantForm";
+import useAuth from "../../../hooks/useAuth";
 
 const AddPlant = () => {
+  const { user } = useAuth();
+  //handle form submit
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const description = form.description.value;
+    const category = form.category.value;
+    const price = parseFloat(form.price.value);
+    const quantity = parseInt(form.quantity.value);
+
+    const image = form.image.files[0];
+    const imageUrl = await imageUpload(image);
+
+      // seller info
+      const seller = {
+        name: user?.displayName,
+        image: user?.photoURL,
+        email: user?.email,
+      }
+
+      // Create plant data object
+    const plantData = {
+      name,
+      category,
+      description,
+      price,
+      quantity,
+      image: imageUrl,
+      seller,
+    }
+    console.table(plantData)
+  };
   return (
     <div>
       <Helmet>
@@ -9,9 +45,9 @@ const AddPlant = () => {
       </Helmet>
 
       {/* Form */}
-      <AddPlantForm />
+      <AddPlantForm handleSubmit={handleSubmit} />
     </div>
-  )
-}
+  );
+};
 
-export default AddPlant
+export default AddPlant;
